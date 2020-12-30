414d27cf1cf0291843765a0f82bcf820807f9e51
goto-satoshi0112 <73925600+goto-satoshi0112@users.noreply.github.com>
2020-12-30 21:50:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -47,11 +47,11 @@ $(function () {
   });
 
   const $submitBtn = $('#js-submit');
-  $('#form input,#form textarea').on('change', function () {
+  $('#form input, #form textarea').on('change', function () {
     if (
       $('#form input[type="text"]').val() !== "" &&
       $('#form input[type="email"]').val() !== "" &&
-      $('#form input[type="checkbox"]').val() !== "" &&
+      $('#form textarea').val() !== "" &&
       $('#form #privacyCheck').prop('checked') === true
     ) {
       $submitBtn.prop('disabled', false);
